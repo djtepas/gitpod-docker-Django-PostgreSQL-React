fd4dfcdc47981f253437de3ba2c94cf36854f12c
djtepas <djtepas@gmail.com>
2020-12-20 19:31:22 +0000

    Testing bash script

--- install-packages.sh
+++ install-packages.sh
@@ -4,21 +4,23 @@
 # script. Every bash script you write should include this. see
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/ for
 # details.
-set -euo pipefail
+# set -euo pipefail
 
-# Tell apt-get we're never going to be able to give manual
-# feedback:
-export DEBIAN_FRONTEND=noninteractive
+# # Tell apt-get we're never going to be able to give manual
+# # feedback:
+# export DEBIAN_FRONTEND=noninteractive
 
-# # Update the package listing, so we know that package exist:
-# su apt-get update
+# # # Update the package listing, so we know that package exist:
+# # su apt-get update
 
-# # Install security updates:
-# su apt-get -y upgrade
+# # # Install security updates:
+# # su apt-get -y upgrade
 
-# Install a new packaage, without unnecessary recommended packages:
-apt-get -y install --no-install-recommends syslog-ng
+# # Install a new packaage, without unnecessary recommended packages:
+# apt-get -y install --no-install-recommends syslog-ng
 
-# Delete cached files we don't need anymore:
-apt-get clean
-rm -rf /var/lib/apt/lists/*
+# # Delete cached files we don't need anymore:
+# apt-get clean
+# rm -rf /var/lib/apt/lists/*
+
+echo "Hello World."
