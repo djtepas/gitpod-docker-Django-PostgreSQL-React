71060cddf8115c7fdca4fa6dceb41dae203d140d
djtepas <djtepas@gmail.com>
2020-12-20 20:44:32 +0000

    django python script

--- script_setup_django.py
+++ script_setup_django.py
@@ -1,4 +1,23 @@
 import os
+from subprocess import Popen
 
-path = os.getcwd()
-print(f'The current working directory is {path}')
+dir_list = ['backend', 'frontend']
+
+def create_dir(dir_list):
+    '''
+    Creates directories from given list.
+    @param: dir_list python list of string directory names.
+    '''
+    path = os.getcwd()
+    for item in dir_list:
+        try:
+            os.mkdir(os.path.join(path, item))
+        except OSError:
+            print(f'Creation of the directory {item} failed.')
+        else:
+            print(f'Successfully created the directory {item}.')
+
+if __name__ == '__main__':
+    create_dir(dir_list)
+    Popen("pipenv install django django-cors-headers djangorestframework coverage")
+    Popen("pipenv run django-admin startproject backend .")
