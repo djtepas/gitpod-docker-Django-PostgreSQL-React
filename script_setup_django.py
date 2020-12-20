08843585c28d877157c3112de7fbea393772d0a3
djtepas <djtepas@gmail.com>
2020-12-20 21:22:16 +0000

    Created init script

--- script_setup_django.py
+++ script_setup_django.py
@@ -1,23 +1,9 @@
-import os
 from subprocess import run
 
-dir_list = ['backend', 'frontend']
-
-def create_dir(dir_list):
-    '''
-    Creates directories from given list.
-    @param: dir_list python list of string directory names.
-    '''
-    path = os.getcwd()
-    for item in dir_list:
-        try:
-            os.mkdir(os.path.join(path, item))
-        except OSError:
-            print(f'Creation of the directory {item} failed.')
-        else:
-            print(f'Successfully created the directory {item}.')
 
 if __name__ == '__main__':
-    create_dir(dir_list)
-    run("pipenv install django django-cors-headers djangorestframework coverage")
-    run("pipenv run django-admin startproject backend .")
+    run(["pipenv", "install", "django", "django-cors-headers", "djangorestframework", "coverage"])
+    run(["pipenv", "run", "django-admin", "startproject", "backend", "."])
+    run(["npx", "create-react-app", "frontend", "."])
+    run(["npm", "install", "--prefix", "./frontend", "react-router-dom", "@material-ui/core"])
+    run(['psql', '-f', 'create_db.sql'])
