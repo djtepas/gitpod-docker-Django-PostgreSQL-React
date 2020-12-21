0524b7d8abb114275335d523141f916db77738a3
djtepas <djtepas@gmail.com>
2020-12-21 17:21:23 +0000

    No need to run a virtual env. workspace is a .venv

--- script_setup_django.py
+++ script_setup_django.py
@@ -2,8 +2,9 @@ from subprocess import run
 
 
 if __name__ == '__main__':
-    run(["pipenv", "install", "django", "django-cors-headers", "djangorestframework", "coverage"])
-    run(["pipenv", "run", "django-admin", "startproject", "backend", "."])
+    run(["pip", "install", "django", "django-cors-headers", "djangorestframework", "coverage"])
+    run(["python", "django-admin", "startproject", "backend", "."])
     run(["npx", "create-react-app", "frontend", "."])
     run(["npm", "install", "--prefix", "./frontend", "react-router-dom", "@material-ui/core"])
     run(['psql', '-f', 'create_db.sql'])
+    
